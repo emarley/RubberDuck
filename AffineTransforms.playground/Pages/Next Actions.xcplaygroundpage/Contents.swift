8c324d66084efcfb5c05ba09e462f465e0bc7def
Liz Marley <liz@appcamp4girls.com>
2017-05-27 16:56:30 -0700

    Bettter next actions
    [No code changed.]
    Remove cultural appropriation of ‘spirit animal’.
    Add new exploration idea (negative scaling!)

--- AffineTransforms.playground/Pages/Next Actions.xcplaygroundpage/Contents.swift
+++ AffineTransforms.playground/Pages/Next Actions.xcplaygroundpage/Contents.swift
@@ -2,7 +2,7 @@
 
 // So you think you're all done, eh? Here's some optional quests.
 
-// * Replace the duck with your spirit animal or twitter avatar.
+// * Replace the duck with your twitter avatar.
 
 // * Take a peek in TransformPuzzleVC.swift in the sources folder and try adding your own puzzles.
 
@@ -11,6 +11,8 @@
 // print(duck.transform)
 
 // Then go read more about the math at the bottom of this page: https://developer.apple.com/library/content/documentation/GraphicsImaging/Conceptual/drawingwithquartz2d/dq_affine/dq_affine.html#//apple_ref/doc/uid/TP30001066-CH204-TPXREF101
+
+// * What transform flips the duck so it's facing left?
 
 // * Brainstorm other coding topics that could be taught with a playground.
 
