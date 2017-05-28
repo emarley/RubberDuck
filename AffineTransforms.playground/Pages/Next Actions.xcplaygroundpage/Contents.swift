8879fa8982a7b798bbea5e420f599d3718dba0ca
Liz Marley <liz@appcamp4girls.com>
2017-05-28 12:48:44 -0700

    Next Actions: formatting and SVG note
    [no code changes]

--- AffineTransforms.playground/Pages/Next Actions.xcplaygroundpage/Contents.swift
+++ AffineTransforms.playground/Pages/Next Actions.xcplaygroundpage/Contents.swift
@@ -1,19 +1,24 @@
 //: [Previous](@previous)
 
-// So you think you're all done, eh? Here's some optional quests.
+/*: 
+ 
+ So you think you're all done, eh? Here's some optional quests.
 
-// * Replace the duck with your twitter avatar.
+* Replace the duck with your twitter avatar.
+ 
+* What transform flips the duck so it's facing left?
 
-// * Take a peek in TransformPuzzleVC.swift in the sources folder and try adding your own puzzles.
+* Take a peek in TransformPuzzleVC.swift in the sources folder and try adding your own puzzles.
 
-// * Add the following line of code to the bottom of one of the other pages in this playground.
+* Add the following line of code to the bottom of one of the other pages in this playground.
 
-// print(duck.transform)
+`print(duck.transform)`
 
-// Then go read more about the math at the bottom of this page: https://developer.apple.com/library/content/documentation/GraphicsImaging/Conceptual/drawingwithquartz2d/dq_affine/dq_affine.html#//apple_ref/doc/uid/TP30001066-CH204-TPXREF101
+Then go read more about the math in [Apple's reference](https://developer.apple.com/library/content/documentation/GraphicsImaging/Conceptual/drawingwithquartz2d/dq_affine/dq_affine.html)
+ 
+For extreme bonus points, note that it's the same as the transforms used in the [SVG image format](https://www.w3.org/TR/SVG/coords.html#EstablishingANewUserSpace), even though they lay out the matrices differently.
 
-// * What transform flips the duck so it's facing left?
+* Brainstorm other coding topics that could be taught with a playground.
 
-// * Brainstorm other coding topics that could be taught with a playground.
-
-// * Get in touch with me: @emarley or lizamarley@gmail.com
+* Get in touch with me: @emarley or lizamarley@gmail.com
+*/
