b7630aeaa426c33bf779993a4dd005b236eba02d
Liz Marley <liz@appcamp4girls.com>
2017-05-28 13:10:01 -0700

    1 puzzle per page.

---
+++ AffineTransforms.playground/Pages/More Translation.xcplaygroundpage/Contents.swift
@@ -0,0 +1,23 @@
+//: [Previous](@previous)
+
+// Created by Liz Marley
+// March 2017
+
+import UIKit
+import PlaygroundSupport
+
+let vc = TransformPuzzleVC()
+vc.topLeftView.backgroundColor = UIColor.green
+PlaygroundPage.current.liveView = vc
+vc.hideButtons()
+
+vc.start(puzzle: 1)
+
+let duck = vc.duck
+
+//: Edit the following line of code to align the ducks...
+duck.transform = CGAffineTransform(translationX: 0, y: 0)
+
+//: Once you've got your duck to its shadow, tap `Next` to move on to the next coding challenge.
+
+//: [Next](@next)
