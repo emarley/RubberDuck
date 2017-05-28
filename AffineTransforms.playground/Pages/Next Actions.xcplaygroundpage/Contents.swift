e73bcd633ecb30ece472ad19706d4f26a9d71c2e
Liz Marley <liz@appcamp4girls.com>
2017-05-28 14:29:23 -0700

    Revised Next Actions…again
    also, turn rendered markdown back on

--- AffineTransforms.playground/Pages/Next Actions.xcplaygroundpage/Contents.swift
+++ AffineTransforms.playground/Pages/Next Actions.xcplaygroundpage/Contents.swift
@@ -1,24 +1,27 @@
-//: [Previous](@previous)
-
 /*: 
+ [Previous](@previous)
  
  So you think you're all done, eh? Here's some optional quests.
 
-* Replace the duck with your twitter avatar.
+ * Replace the duck with your social media avatar or a favorite Sesame Street character.
  
-* What transform flips the duck so it's facing left?
+ * What transform flips the duck so it's facing left?
 
-* Take a peek in TransformPuzzleVC.swift in the sources folder and try adding your own puzzles.
+ * Volunteer to help someone near you. Explaining something is a great way to reinforce your own knowledge.
 
-* Add the following line of code to the bottom of one of the other pages in this playground.
+ * Brainstorm other coding topics that could be taught with a playground.
 
-`print(duck.transform)`
+ * Team up with someone on the other platform and talk about the differences between Xcode and iPad playground user experiences.
 
-Then go read more about the math in [Apple's reference](https://developer.apple.com/library/content/documentation/GraphicsImaging/Conceptual/drawingwithquartz2d/dq_affine/dq_affine.html)
+* *Xcode*: Take a peek in TransformPuzzleVC.swift in the `Sources` folder and add your own puzzles.
  
-For extreme bonus points, note that it's the same as the transforms used in the [SVG image format](https://www.w3.org/TR/SVG/coords.html#EstablishingANewUserSpace), even though they lay out the matrices differently.
+* *Xcode*: Add the following line of code to the bottom of one of the other pages in this playground. Notice the six numbers.
 
-* Brainstorm other coding topics that could be taught with a playground.
+    `print(duck.transform)`
 
-* Get in touch with me: @emarley or lizamarley@gmail.com
+    Then go read more about the math in [Apple's reference](https://developer.apple.com/library/content/documentation/GraphicsImaging/Conceptual/drawingwithquartz2d/dq_affine/dq_affine.html)
+ 
+    Note that it's the same as the transforms used in the [SVG image format](https://www.w3.org/TR/SVG/coords.html#EstablishingANewUserSpace), even though they lay out the matrices differently.
+ 
+* Get in touch with me: [@emarley](https://twitter.com/@emarley) or [lizamarley@gmail.com](mailto:lizamarley@gmail.com)
 */
