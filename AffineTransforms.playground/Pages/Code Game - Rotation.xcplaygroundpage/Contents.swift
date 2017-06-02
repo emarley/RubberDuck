d7135acd5b8c4f04a65f0097ef66df773733fe43
Liz Marley <liz@appcamp4girls.com>
2017-06-01 20:58:45 -0700

    Fix link

--- AffineTransforms.playground/Pages/Code Game - Rotation.xcplaygroundpage/Contents.swift
+++ AffineTransforms.playground/Pages/Code Game - Rotation.xcplaygroundpage/Contents.swift
@@ -14,7 +14,7 @@ let duck = vc.duck
 //: Edit the following line of code to get the duck aligned with its shadow.
 duck.transform = CGAffineTransform(rotationAngle: CGFloat.pi * 1/4)
 
-//: Where did the 4 and the pi come from? These angles are in radians. If you think in degrees, here's a conversion chart: https://commons.wikimedia.org/wiki/File:Unit_circle_angles_color.svg
+//: Where did the 4 and the pi come from? These angles are in radians. If you think in degrees, [here's a conversion chart](https://commons.wikimedia.org/wiki/File:Unit_circle_angles_color.svg).
 
 
 //: [Next](@next)
