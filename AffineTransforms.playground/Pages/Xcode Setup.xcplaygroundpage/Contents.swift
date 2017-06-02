b7d9ca1e80f1385dde98df14c791d9de2c9d7ec1
Liz Marley <liz@appcamp4girls.com>
2017-06-01 20:45:44 -0700

    Added iPad setup instructions

--- AffineTransforms.playground/Pages/Xcode Setup.xcplaygroundpage/Contents.swift
+++ AffineTransforms.playground/Pages/Xcode Setup.xcplaygroundpage/Contents.swift
@@ -1,18 +1,22 @@
 /*:
-  ## Xcode Setup Instructions
- (If you are on an iPad, please tap `Next` at the bottom of this page to get on with the playground.)
- 
- * Click on this text, then open the **Editor** menu and choose **Show Rendered Markup**. This text should stop looking like markdown or code comments.
- 
- * **View > Assistant Editor > Show Assistant Editor**
- 
- * At the top of the Assistant Editor, choose **Timeline** (not **Manual**). You should see a duck on the right hand half of your screen.
- 
+ ## iPad Setup Instructions
+
+ * Tap 'Run My Code' in the lower right corner to split the screen and see a graphic view with a duck.
+
+ * When you're finished with each page, tap the red 'Next' in the bottom left to move on to the next exercise. (You should skip the Xcode Setup instructions and tap 'Next' now.)
+
+ ## Xcode Setup Instructions
+
  * If you are running Xcode 8.3 (or newer?), quit Xcode, run the following command in Terminal, and then relaunch Xcode.
- 
-```
+
+ ```
  defaults write com.apple.dt.xcode IDEPlaygroundDisableSimulatorAlternateFramebuffer -bool YES
-```
+ ```
+
+ * **View > Assistant Editor > Show Assistant Editor**
+
+ * At the top of the Assistant Editor, choose **Timeline** (not **Manual**). You should see a duck on the right hand half of your screen.
+
  [Next](@next)
  */
 
